ecb3d5247ff9ac04627e60bbd76888a7a036c00a
Michael <mterekhov@cleverbits.co>
2015-05-06 18:19:31 +0300

    trash commit

--- FitmeupChart/GMCoreDataHelper.m
+++ FitmeupChart/GMCoreDataHelper.m
@@ -4,7 +4,7 @@
 //
 //  Created by Anton Gubarenko on 22.04.15.
 //  Copyright (c) 2015 Anton Gubarenko. All rights reserved.
-//
+
 
 //=============================================================================
 
